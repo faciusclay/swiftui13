a26d5b750f698a60696fd7d12864308d33580e28
Angel Miranda <angelr.miranda.rios@gmail.com>
2020-06-19 13:16:36 -0500

    Stacks
    Carga inicial de los stacks

---
+++ 03-stacks/03-stacks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  03-stacks
+//
+//  Created by Angel Miranda on 19/06/20.
+//  Copyright © 2020 Angel Miranda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
