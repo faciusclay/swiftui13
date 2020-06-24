746cf0e5e927734ef8996b37bed3e74514840551
Angel Miranda <angelr.miranda.rios@gmail.com>
2020-06-24 11:55:53 -0500

    Carga inicial

---
+++ 05-Buttons/05-Buttons/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  05-Buttons
+//
+//  Created by Angel Miranda on 24/06/20.
+//  Copyright © 2020 Angel Miranda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
