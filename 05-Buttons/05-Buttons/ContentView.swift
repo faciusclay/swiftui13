6b443efac876246ae9aa061980fd9bfedd38b8d3
Angel Miranda <angelr.miranda.rios@gmail.com>
2020-06-25 17:43:07 -0500

    Estilos de botones
    Se crea un estilo para los botones y un botón que se gira al momento de tocarlo

--- 05-Buttons/05-Buttons/ContentView.swift
+++ 05-Buttons/05-Buttons/ContentView.swift
@@ -34,12 +34,7 @@ struct ContentView: View {
                 print("Botón ícono pulsado")
             }){
                 Image(systemName: "trash")
-                    .padding()
-                    .background(Color.red)
-                    .clipShape(Circle())
-                    .font(.largeTitle)
-                    .foregroundColor(.white)
-            }
+            }.buttonStyle(BotonGirado())
             
             Button(action: {
                 print("Botón texto e ícono pulsado")
@@ -54,9 +49,44 @@ struct ContentView: View {
                 .background(LinearGradient(gradient: Gradient(colors:[Color.orange, Color.red]), startPoint: .top, endPoint: .bottom))
                     .font(.largeTitle)
                     .foregroundColor(.white)
-            .cornerRadius(5)
-                .shadow(radius: 10, x: 5, y:5)
+            .cornerRadius(10)
+                .shadow(radius: 5, x: 5, y:5)
             }
+            
+            Button(action: {
+                print("Botón editar infinito pulsado")
+            }){
+                HStack{
+                    Image(systemName: "square.and.pencil")
+                    Text("Editar")
+                        .fontWeight(.regular)
+                        .font(.system(.largeTitle, design: .rounded))
+                }
+            }.buttonStyle(BasicButtonStyle())
+            
+            
+            Button(action: {
+                print("Botón compartir infinito pulsado")
+            }){
+                HStack{
+                    Image(systemName: "square.and.arrow.up")
+                    Text("Compartir")
+                        .fontWeight(.regular)
+                        .font(.system(.largeTitle, design: .rounded))
+                }
+            }.buttonStyle(BasicButtonStyle())
+            
+            Button(action: {
+                print("Botón eliminar infinito pulsado")
+            }){
+                HStack{
+                    Image(systemName: "trash")
+                    Text("Eliminar")
+                        .fontWeight(.regular)
+                        .font(.system(.largeTitle, design: .rounded))
+                }
+            }.buttonStyle(BasicButtonStyle())
+            
         }
     }
 }
@@ -64,5 +94,33 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct BasicButtonStyle: ButtonStyle{
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+        .frame(minWidth:0, maxWidth: .infinity)
+        .padding()
+            .background(LinearGradient(gradient: Gradient(colors:[Color.orange, Color.red]), startPoint: .top, endPoint: .bottom))
+            .font(.largeTitle)
+            .foregroundColor(.white)
+        .cornerRadius(50)
+        .shadow(radius: 5, x: 5, y:5)
+        .padding(.horizontal)
+            .scaleEffect(configuration.isPressed ? 0.8 : 1.0)
+    }
+}
+
+
+struct BotonGirado: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+        .padding()
+        .background(Color.red)
+        .clipShape(Circle())
+        .font(.largeTitle)
+        .foregroundColor(.white)
+            .rotationEffect(configuration.isPressed ? Angle(degrees: 90) : Angle(degrees: 0))
     }
 }
