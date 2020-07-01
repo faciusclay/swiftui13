fa0e9efb24a7edd16aec2235c19f1877285d15a4
Angel Miranda <angelr.miranda.rios@gmail.com>
2020-07-01 17:39:50 -0500

    Draw carga inicial

---
+++ 07-draw/07-draw/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  07-draw
+//
+//  Created by Angel Miranda on 01/07/20.
+//  Copyright © 2020 Angel Miranda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
