a26efca8a71bc467a819a2aa0320fd47e9d77034
Angel Miranda <angelr.miranda.rios@gmail.com>
2020-07-01 18:39:19 -0500

    Explicación del punto de control de la curva

---
+++ 07-draw/07-draw/CurveView.swift
@@ -0,0 +1,37 @@
+//
+//  CurveView.swift
+//  07-draw
+//
+//  Created by Angel Miranda on 01/07/20.
+//  Copyright © 2020 Angel Miranda. All rights reserved.
+//
+
+import SwiftUI
+
+struct CurveView: View {
+    var body: some View {
+        ZStack {
+            Path(){ path in
+                path.move(to: CGPoint(x: 60, y: 130))
+                path.addLine(to: CGPoint(x: 130, y: 0))
+                path.addLine(to: CGPoint(x: 200, y: 130))
+            }.stroke(Color.red, lineWidth: 5)
+            
+            Path(){ path in
+                path.move(to: CGPoint(x: 30, y: 130))
+                path.addLine(to: CGPoint(x: 60, y: 130))
+                path.addQuadCurve(to: CGPoint(x: 200, y: 130), control: CGPoint(x: 130, y: 0))
+                path.addLine(to: CGPoint(x: 230, y: 130))
+                path.addLine(to: CGPoint(x: 230, y: 250))
+                path.addLine(to: CGPoint(x: 30, y: 250))
+            }
+            .fill(Color.blue)
+        }
+    }
+}
+
+struct CurveView_Previews: PreviewProvider {
+    static var previews: some View {
+        CurveView()
+    }
+}
