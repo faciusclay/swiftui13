acc81b9af99e80b7f313b5bc029e0b4cb6e51085
Angel Miranda <angelr.miranda.rios@gmail.com>
2020-07-01 19:37:01 -0500

    Gráfica de pastel

---
+++ 07-draw/07-draw/PieView.swift
@@ -0,0 +1,61 @@
+//
+//  PieView.swift
+//  07-draw
+//
+//  Created by Angel Miranda on 01/07/20.
+//  Copyright © 2020 Angel Miranda. All rights reserved.
+//
+
+import SwiftUI
+
+struct PieView: View {
+    var body: some View {
+        ZStack{
+            Path(){ path in
+                path.move(to: CGPoint(x: 200, y: 250))
+                path.addArc(center: CGPoint(x: 200, y: 250), radius: 160, startAngle: Angle(degrees: 0), endAngle: Angle(degrees: 200), clockwise: true)
+            }
+            .fill(Color(.systemBlue))
+            
+            Path(){ path in
+                path.move(to: CGPoint(x: 200, y: 250))
+                path.addArc(center: CGPoint(x: 200, y: 250), radius: 160, startAngle: Angle(degrees: 200), endAngle: Angle(degrees: 160), clockwise: true)
+            }
+            .fill(Color(.systemOrange))
+            .offset(x: -30, y:0)
+            
+            Path(){ path in
+                path.move(to: CGPoint(x: 200, y: 250))
+                path.addArc(center: CGPoint(x: 200, y: 250), radius: 160, startAngle: Angle(degrees: 200), endAngle: Angle(degrees: 160), clockwise: true)
+                path.closeSubpath()
+            }
+            .stroke(Color(.systemRed), lineWidth: 3)
+            .offset(x: -30, y:0)
+            .overlay(Text("11.1%")
+                .font(.system(.largeTitle, design: .rounded))
+                .bold()
+                .foregroundColor(.white)
+                .offset(x: -140, y: -160)
+            )
+            
+            Path(){ path in
+                           path.move(to: CGPoint(x: 200, y: 250))
+                           path.addArc(center: CGPoint(x: 200, y: 250), radius: 160, startAngle: Angle(degrees: 160), endAngle: Angle(degrees: 110), clockwise: true)
+                       }
+            .fill(Color(.systemIndigo))
+            
+            Path(){ path in
+                           path.move(to: CGPoint(x: 200, y: 250))
+                           path.addArc(center: CGPoint(x: 200, y: 250), radius: 160, startAngle: Angle(degrees: 110), endAngle: Angle(degrees: 360), clockwise: true)
+                       }
+            .fill(Color(.systemTeal))
+        }
+        
+    }
+}
+
+struct PieView_Previews: PreviewProvider {
+    static var previews: some View {
+        PieView()
+    }
+}
