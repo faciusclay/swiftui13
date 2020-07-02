5599f67da81499406b59c9696ee5a486ad877dac
Angel Miranda <angelr.miranda.rios@gmail.com>
2020-07-01 20:31:47 -0500

    uso de Shape
    Se hace uso del protocolo Shape para crear una forma definida por nosotros

--- 07-draw/07-draw/CurveView.swift
+++ 07-draw/07-draw/CurveView.swift
@@ -11,22 +11,29 @@ import SwiftUI
 struct CurveView: View {
     var body: some View {
         ZStack {
-            Path(){ path in
-                path.move(to: CGPoint(x: 60, y: 130))
-                path.addLine(to: CGPoint(x: 130, y: 0))
-                path.addLine(to: CGPoint(x: 200, y: 130))
-            }
-            .stroke(Color.orange, lineWidth: 5)
+//            Path(){ path in
+//                path.move(to: CGPoint(x: 60, y: 130))
+//                path.addLine(to: CGPoint(x: 130, y: 0))
+//                path.addLine(to: CGPoint(x: 200, y: 130))
+//            }
+//            .stroke(Color.orange, lineWidth: 5)
             
-            Path(){ path in
-                path.move(to: CGPoint(x: 30, y: 130))
-                path.addLine(to: CGPoint(x: 60, y: 130))
-                path.addQuadCurve(to: CGPoint(x: 200, y: 130), control: CGPoint(x: 130, y: 0))
-                path.addLine(to: CGPoint(x: 230, y: 130))
-                path.addLine(to: CGPoint(x: 230, y: 250))
-                path.addLine(to: CGPoint(x: 30, y: 250))
+//            Path(){ path in
+//                path.move(to: CGPoint(x: 60, y: 130))
+//                path.addQuadCurve(to: CGPoint(x: 200, y: 130), control: CGPoint(x: 130, y: 0))
+//                path.addRect(CGRect(x: 30, y: 130, width: 200, height: 120))
+//            }
+//            .fill(Color.blue)
+            Button(action: {
+                
+            }){
+                Text("Cartelito")
+                    .font(.system(.title, design: .rounded))
+                    .bold()
+                    .foregroundColor(Color.white)
+                    .frame(width: 200, height: 50)
+                    .background(FunnyLabel(percCircle: 0.8, percAltura: 0.4).fill(Color.blue))
             }
-            .fill(Color.blue)
             
             Path(){ path in
                 path.move(to: CGPoint(x: 30, y: 130))
@@ -45,5 +52,21 @@ struct CurveView: View {
 struct CurveView_Previews: PreviewProvider {
     static var previews: some View {
         CurveView()
+    }
+}
+
+struct FunnyLabel: Shape {
+    
+    var percCircle: CGFloat = 1.0
+    var percAltura: CGFloat = 1.0
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.size.width * (1-percCircle)/2, y: 0))
+        path.addQuadCurve(to: CGPoint(x: rect.size.width * (1-(1-percCircle)/2), y: 0), control: CGPoint(x: rect.size.width/2, y: -(rect.size.width * percAltura/2)))
+        path.addRect(CGRect(x: 0, y: 0, width: rect.size.width, height: rect.size.height))
+        
+        return path
     }
 }
--- 07-draw/07-draw/PieView.swift
+++ 07-draw/07-draw/PieView.swift
@@ -11,12 +11,15 @@ import SwiftUI
 struct PieView: View {
     var body: some View {
         ZStack{
+            
+            //Superior
             Path(){ path in
                 path.move(to: CGPoint(x: 200, y: 250))
                 path.addArc(center: CGPoint(x: 200, y: 250), radius: 160, startAngle: Angle(degrees: 0), endAngle: Angle(degrees: 200), clockwise: true)
             }
             .fill(Color(.systemBlue))
             
+            //Destacado
             Path(){ path in
                 path.move(to: CGPoint(x: 200, y: 250))
                 path.addArc(center: CGPoint(x: 200, y: 250), radius: 160, startAngle: Angle(degrees: 200), endAngle: Angle(degrees: 160), clockwise: true)
@@ -38,12 +41,14 @@ struct PieView: View {
                 .offset(x: -140, y: -160)
             )
             
+            //Inferior izquierda
             Path(){ path in
                            path.move(to: CGPoint(x: 200, y: 250))
                            path.addArc(center: CGPoint(x: 200, y: 250), radius: 160, startAngle: Angle(degrees: 160), endAngle: Angle(degrees: 110), clockwise: true)
                        }
             .fill(Color(.systemIndigo))
             
+            //Inferior derecha
             Path(){ path in
                            path.move(to: CGPoint(x: 200, y: 250))
                            path.addArc(center: CGPoint(x: 200, y: 250), radius: 160, startAngle: Angle(degrees: 110), endAngle: Angle(degrees: 360), clockwise: true)
