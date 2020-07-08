6a6d320bfc127be41896f908234669d2d71b690d
Angel Miranda <angelr.miranda.rios@gmail.com>
2020-07-07 19:35:12 -0500

    08 animaciones
    Carga inicial

---
+++ 08-anims/08-anims/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  08-anims
+//
+//  Created by Angel Miranda on 07/07/20.
+//  Copyright © 2020 Angel Miranda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
